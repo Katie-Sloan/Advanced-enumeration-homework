36bb7b64cc7744071e7deb4650a72546b9aa61b8
Katie Sloan <katie.a.sloan@gmail.com>
2020-11-18 22:34:24 +0000

    started anagram finder

--- anagram_finder/anagram_finder.js
+++ anagram_finder/anagram_finder.js
@@ -1,9 +1,14 @@
 const AnagramFinder = function (word) {
-
+    this.word = word;
 }
 
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-
+    const sameLengthWordsSet = new Set(this.word.toLowerCase()).size === this.word.length;
+    sameLengthWordsArray = Array.from(sameLengthWordsSet);
+    
+    // return sameLengthWordsArray
+    // .sort()
+    
 }
 
 module.exports = AnagramFinder;
