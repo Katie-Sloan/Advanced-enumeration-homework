d0b4a1dafa9989f9fb8023b757757293d117fa00
Katie Sloan <katie.a.sloan@gmail.com>
2020-11-18 21:04:14 +0000

    completed pangram finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,14 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase;
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  const isPangram = (string) => {
+    string = string.toLowerCase();
+    return this.alphabet.every(x => string.includes(x));
+  }
+  return isPangram(this.phrase);
 }
 
 module.exports = PangramFinder;
