a548e2590bac033d10c37cc15190a0cb4c717e0c
Katie Sloan <katie.a.sloan@gmail.com>
2020-11-18 21:18:51 +0000

    completed isogram finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,9 @@
 const IsogramFinder = function (word) {
-
+    this.word = word;
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    return new Set(this.word.toLowerCase()).size === this.word.length;
 }
 
 module.exports = IsogramFinder;
